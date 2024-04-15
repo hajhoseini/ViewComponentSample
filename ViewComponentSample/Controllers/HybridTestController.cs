0f64969068a62fad5ec98bb7a6ddb8315ed84daa
a.hoseni <a.hoseni@DPK-115>
2024-04-15 16:33:57 +0330

    Hybrid Controller

---
+++ ViewComponentSample/Controllers/HybridTestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentSample.Controllers
+{
+    [ViewComponent(Name = "Test")]
+    public class HybridTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Invoke()
+        {
+            return "";
+        }
+    }
+}
